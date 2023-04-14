1a123ae9cac4589e406ae09bc8130c0dec44d17f
Jeff Nelson <jpnelson85@gmail.com>
2023-04-14 11:13:38 -0500

    add candidates method in election class

---
+++ lib/election.rb
@@ -0,0 +1,25 @@
+class Election
+  attr_reader :year,
+              :races
+
+  def initialize(year)
+    @year = year
+    @races = []
+  end
+  
+  def add_race(race)
+    @races << race
+  end
+
+  def candidates
+    @races.map do |race|
+      race.candidates.map do |candidate|
+        candidate
+      end
+    end.flatten
+  end
+
+  def vote_counts
+
+  end
+end
---
+++ spec/election_spec.rb
@@ -0,0 +1,44 @@
+require './lib/candidate'
+require './lib/race'
+require './lib/election'
+
+RSpec.describe Election do
+  it 'exists' do
+    election1 = Election.new("2024")
+    expect(election1).to be_a(Election)
+  end
+
+  it 'has attributes' do
+    election1 = Election.new("2024")
+    expect(election1.year).to eq("2024")
+    expect(election1.races).to eq([])
+  end
+
+  it 'add races in election' do
+    election1 = Election.new("2024")
+    race = Race.new("Texas Governor")
+    candidate1 = race.register_candidate!({name: "Diana D", party: :democrat})
+    candidate2 = race.register_candidate!({name: "Roberto R", party: :republican})
+    expect(election1.races).to eq([])
+    election1.add_race(race)
+    expect(election1.races).to eq([race])
+  end
+
+  it 'candidates method' do
+    election1 = Election.new("2024")
+    race = Race.new("Texas Governor")
+    candidate1 = race.register_candidate!({name: "Diana D", party: :democrat})
+    candidate2 = race.register_candidate!({name: "Roberto R", party: :republican})
+    election1.add_race(race)
+    expect(election1.candidates).to eq([candidate1, candidate2])
+  end
+
+  
+
+end
+
+# diana = Candidate.new({name: "Diana D", party: :democrat})
+# diana.vote_for!
+# race = Race.new("Texas Governor")
+# candidate1 = race.register_candidate!({name: "Diana D", party: :democrat})
+# candidate2 = race.register_candidate!({name: "Roberto R", party: :republican})
