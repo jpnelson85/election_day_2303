c54862537cd86c32535ad97487f2e4cfc394772b
Jeff Nelson <jpnelson85@gmail.com>
2023-04-14 10:17:37 -0500

    add first two tests race spec

---
+++ spec/race_spec.rb
@@ -0,0 +1,15 @@
+require './lib/candidate'
+require './lib/race'
+
+RSpec.describe Race do
+  it 'exists' do
+    race = Race.new("Texas Governor")
+    expect(race).to be_a(Race)
+  end
+
+  it 'has attributes' do
+    race = Race.new("Texas Governor")
+    expect(race.office).to eq("Texas Governor")
+    expect(race.candidates).to eq([])
+  end
+end
