4aefb13d40a417b0501e3893b38d16d54c51c514
Jeff Nelson <jpnelson85@gmail.com>
2023-04-14 10:07:04 -0500

    add exists and attributes tests

---
+++ spec/election_spec.rb
@@ -0,0 +1,15 @@
+require './lib/candidate'
+
+RSpec.describe Candidate do
+  it 'exists' do
+    diana = Candidate.new({name: "Diana D", party: :democrat})
+    expect(diana).to be_a(Candidate)
+  end
+
+  it 'has attributes' do
+    diana = Candidate.new({name: "Diana D", party: :democrat})
+    expect(diana.name).to eq("Diana D")
+    expect(diana.party).to eq(:democrat)
+    expect(diana.votes).to eq(0)
+  end
+end
