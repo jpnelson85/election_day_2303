81426c7e5f4c8115c01225c7a8639a43323947fa
Jeff Nelson <jpnelson85@gmail.com>
2023-04-14 10:13:44 -0500

    finish iteration 1

--- lib/candidate.rb
+++ lib/candidate.rb
@@ -1,1 +1,15 @@
-class
+class Candidate
+  attr_reader :name,
+              :party,
+              :votes
+
+  def initialize(details)
+    @name = details[:name]
+    @party = details[:party]
+    @votes = 0
+  end
+
+  def vote_for!
+    @votes += 1
+  end
+end
--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe Candidate do
     expect(diana.party).to eq(:democrat)
     expect(diana.votes).to eq(0)
   end
+
+  it 'vote_for! method' do
+    diana = Candidate.new({name: "Diana D", party: :democrat})
+    expect(diana.votes).to eq(0)
+    diana.vote_for!
+    diana.vote_for!
+    diana.vote_for!
+    expect(diana.votes).to eq(3)
+    diana.vote_for!
+    expect(diana.votes).to eq(4)
+  end
 end
